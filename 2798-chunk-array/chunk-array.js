6f68d57270535df1cb2884219178bec9a62e8e9f
sachingiri58 <sachinggiri01@gmail.com>
2025-08-22 23:21:08 +0530

    Time: 44 ms (61.98%) | Memory: 56.4 MB (38.93%) - LeetSync

---
+++ 2798-chunk-array/chunk-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < arr.length) {
+    chunked.push(arr.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+};
+
+
