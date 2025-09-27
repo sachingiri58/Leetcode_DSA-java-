f8569b55ea7e01dc6aef692f7d8728540f28bdd0
sachingiri58 <sachinggiri01@gmail.com>
2025-09-27 22:49:50 +0530

    Time: 15 ms (47.63%) | Memory: 61.4 MB (6.31%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(nums[i])) {
+                return true;
+            } else {
+                hm.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
